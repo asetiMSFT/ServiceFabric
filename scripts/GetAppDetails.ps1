ab91fa241471b2461c691ab4c76e43c7986d18a9
asetiMSFT <43455137+asetiMSFT@users.noreply.github.com>
2018-12-13 10:43:09 -0800

    Update GetAppDetails.ps1
    Added Description, added certThumbprint parameter, fixed few bugs related with console output and parameter checking.

--- scripts/GetAppDetails.ps1
+++ scripts/GetAppDetails.ps1
@@ -1,4 +1,38 @@
-﻿#Requires -Version 3.0
+﻿######################################################################################################################################################
+# GetAppDetails.ps1
+# Copyright (c) 2018 - Microsoft Corp.
+#
+# Author(s); Andrew Setiawan
+#
+# Description:
+# Powershell script to dump all 5 layers in the cluster as shown in Service Fabric Explorer (SFX).
+# This script was created to help customer to enumerate all properties of application and the underlaying objects.
+#
+# Usage sample:
+# 
+# ./GetAppDetails.ps1 -subscriptionId $subId -clusterFQDN $clusterFQDN -doAzureLogin -useTableFormat -outputFile 'C:\TEMP\Report1.txt'
+# ./GetAppDetails.ps1 -subscriptionId $subId -clusterFQDN $clusterFQDN -certThumbprint $thumbprintOverride 
+#
+# Parameters:
+# -subscriptionId : your subscription ID (without enclosing braces).
+# -clusterFQDN    : your Cluster's Fully Qualified Domain Name. This is to help the script to locate the cert in your certificate store (User's MY store, not LOCAL COMPUTER's MY store).
+# -certThumbprint : optional - cert Thumbprint to use if you have multiple certs with the same clusterFQDN as subject, the script will use this thumbprint instead.
+# -outputFile     : optional - Filename/path for the output of this script.
+# -doAzureLogin   : optional - by default, the script won’t attempt to do login to your azure account. You must specify this switch if you’re opening powershell for the first time to run this script.
+# -useTableFormat : optional - when specified, this will dump the content in table (horizontal) formatting.
+
+# Notes:
+# - if you have multiple certificates with the same subject with your clusterFQDN on it, the script will not be able to determine which one to use and assume the first one to use.
+#   In future, we may update the script to improve this limitation.
+# - for string parameters, please enclose them with quotes.
+#
+# History:
+# 12/3/2018 - Created.
+# 12/13/2018 - Added Description, added certThumbprint parameter, fixed few bugs related with console output and parameter checking.
+######################################################################################################################################################
+
+
+#Requires -Version 3.0
 Param(
     [Parameter(Mandatory=$true)] 
     [ValidateNotNullOrEmpty()]
@@ -10,20 +44,27 @@ Param(
 
     [Parameter(Mandatory=$false)] 
     [ValidateNotNullOrEmpty()]
+    [string] $certThumbprint,
+
+    [Parameter(Mandatory=$false)] 
+    [ValidateNotNullOrEmpty()]
     [string] $outputFile="AppDetails.txt",
 
     [Parameter(Mandatory=$false)] 
     [ValidateNotNullOrEmpty()]
-    [switch] $doAzureLogin,
+    [switch] $doAzureLogin = $false,
 
     [Parameter(Mandatory=$false)] 
     [ValidateNotNullOrEmpty()]
-    [switch] $useTableFormat
+    [switch] $useTableFormat = $false
 
 )
 
 Set-StrictMode -Version 3
 $sep0 = '===================================================================================================================================='
+Write-Host $sep0 
+Write-Host 'GetAppDetails.ps1' 
+Write-Host $sep0 
 
 Write-Host "Attempting to connect to Azure using Subscription: " $subscriptionId "..."
 
@@ -33,15 +74,20 @@ $sep0 | Out-File -filepath $outputFile -Append
 $msg = 'SubscriptionId: ' + $subscriptionId
 $msg | Out-File -filepath $outputFile -Append
 
-if ($doAzureLogin -ne $false) 
+if ($doAzureLogin.IsPresent) 
 {
+    Write-Host 'SubscriptionId: ' + $subscriptionId
     Connect-AzureRmAccount
+    Set-AzureRmContext -SubscriptionId $subscriptionId
 }
+
+
+Write-Host 'Attempting to connect to cluster: ' $clusterFQDN '...'
 
 Set-AzureRmContext -SubscriptionId $subscriptionId
 
 
-Write-Host 'Attempting to connect to cluster: $clusterFQDN...'
+Write-Host 'Finding certificate for cluster: ' $clusterFQDN '...'
 
 $clusterEndpoint = $clusterFQDN+':19000' 
 $certThumbprints = (Get-ChildItem -Path Cert:\CurrentUser\My | where {$_.Subject -like "*$clusterFQDN*" }).Thumbprint 
@@ -49,13 +95,25 @@ $certThumbprints = (Get-ChildItem -Path Cert:\CurrentUser\My | where {$_.Subject
 #Client Certificate for the cluster MUST already be installed in your user account's certificate store (don't use Local Machine's certificate store!).
 #NOTE: There could be more than one certificate with the same subject. We'll use the first one found.
 Write-Host 'Found Certificate(s):' $certThumbprints 
-$certThumbprintToUse = $certThumbprints[0]
-#NOTE: If you want to use specific cert to use, copy paste that here and it'll override the previously found cert.
-#$certThumbprintToUse = '<copy paste your certificate thumbprint here>'
+if ($certThumbprints.Length > 1)
+{
+    $certThumbprintToUse = $certThumbprints[0]
+}
+else
+{
+    $certThumbprintToUse = $certThumbprints
+}
+
+if (![string]::IsNullOrWhitespace($certThumbprint))
+{
+    Write-Host 'Using the Cert Thumbprint override parameter since it was specified:' $certThumbprint
+    $certThumbprintToUse = $certThumbprint
+} 
 
 Write-Host 'Using this Certificate to connect to cluster:' $certThumbprintToUse 
-$cluster = Connect-ServiceFabricCluster -ConnectionEndpoint $clusterEndpoint -KeepAliveIntervalInSec 10 -X509Credential -ServerCertThumbprint $certThumbprint -FindType FindByThumbprint -FindValue $certThumbprintToUse -StoreLocation CurrentUser -StoreName My  | Out-File -filepath $outputFile -Append
+$cluster = Connect-ServiceFabricCluster -ConnectionEndpoint $clusterEndpoint -KeepAliveIntervalInSec 10 -X509Credential -ServerCertThumbprint $certThumbprintToUse -FindType FindByThumbprint -FindValue $certThumbprintToUse -StoreLocation CurrentUser -StoreName My  | Out-File -filepath $outputFile -Append
 Write-Host ($cluster | Format-Table | Out-String)
+
 
 $apps = Get-ServiceFabricApplication 
 
@@ -222,3 +280,5 @@ Write-Host $msg
 
 $sep0 | Out-File -filepath $outputFile -Append
 Write-Host $sep0
+
+Write-Host 'DONE.'
